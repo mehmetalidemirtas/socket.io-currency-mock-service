0f60e144ebf2a633a6725afb74490bfa87b9b21a
Mehmetali Demirtas <82759834+mehmetalidemirtas@users.noreply.github.com>
2023-05-05 14:57:43 +0300

    Update index.js

--- index.js
+++ index.js
@@ -105,3 +105,6 @@ function calculateChangeRate(previousValue, currentValue) {
 server.listen(3000, () => {
   console.log("listening on :3000");
 });
+app.get('/', (req, res) => {
+  res.send('Mock data service is running');
+});
